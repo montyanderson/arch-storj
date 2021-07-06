321c6bf17f2c0dc9e6840c937ced1c879efc61c2
Monty Anderson <work@montyanderson.net>
2021-07-06 03:19:41 +0100

    run deno container as root

---
+++ generate-PKGBUILD.js
@@ -0,0 +1,42 @@
+(async () => {
+
+	const [ release ] = await (await fetch("https://api.github.com/repos/storj/storj/releases")).json();
+
+	const { name } = release;
+	const { browser_download_url } = release.assets.find(asset => asset.name === "uplink_linux_amd64.zip");
+
+	const output = `
+_pkgname=storj
+pkgname=$_pkgname-bin
+pkgver=${name.slice(1)}
+pkgrel=1
+pkgdesc=""
+arch=("x86_64")
+url=""
+license=("")
+depends=( )
+optdepends=( )
+conflicts=("$_pkgname")
+provides=("$_pkgname")
+source=( ${JSON.stringify(browser_download_url)} )
+noextract=( "uplink_linux_amd64.zip" )
+sha256sums=( "" )
+
+build() {
+        cd "$srcdir/";
+
+        gunzip uplink_linux_amd64.zip -c > uplink;
+        chmod +x uplink;
+}
+
+package() {
+        cd "$srcdir/";
+
+        mkdir -p "$pkgdir/usr/bin/";
+        mv uplink "$pkgdir/usr/bin/";
+}
+	`.trim();
+
+	console.log(output);
+		
+})();
